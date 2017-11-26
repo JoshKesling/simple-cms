d74507efe84e9fb6fd7b3f12b3b029a58770fc38
Josh Kesling <10089440+JoshKesling@users.noreply.github.com>
2017-11-26 19:05:50 +0800

    Add page, section, and subject models

---
+++ app/models/subject.rb
@@ -0,0 +1,9 @@
+class Subject < ApplicationRecord
+  scope :visible, -> { where(visible: true) }
+  scope :invisible, -> { where(visible: false) }
+  scope :sorted, -> { order("subjects.position ASC") }
+  scope :newest_first, -> { order("subjects.created_at DESC") }
+  scope :search, lambda { |query|
+    where(["name LIKE ?", "%#{query}%"])
+  }
+end
