728db7c340cffbeb3878a1ee1dceb69d06747e65
Josh Kesling <10089440+JoshKesling@users.noreply.github.com>
2017-12-01 17:17:00 +0800

    Add form helpers

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -10,6 +10,8 @@ class SectionsController < ApplicationController
 
   def new
     @section = Section.new(name: 'Default')
+    @pages = Page.order('position ASC')
+    @section_count = Section.count + 1
   end
 
   def create
@@ -18,12 +20,15 @@ class SectionsController < ApplicationController
       flash[:notice] = "Section created successfully."
       redirect_to(action: :index)
     else
+      @section_count = Section.count + 1
       render(:new)
     end
   end
 
   def edit
     @section = Section.find(params[:id])
+    @section_count = Section.count
+    @pages = Page.order('position ASC')
   end
 
   def update
@@ -32,6 +37,7 @@ class SectionsController < ApplicationController
       flash[:notice] = "Section updated successfully."
       redirect_to(action: :show, id: @section.id)
     else
+      @pages = Page.order('position ASC')
       flash[:error] = "There was an error updating the section."
       render(:edit)
     end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -10,6 +10,7 @@ class SubjectsController < ApplicationController
 
   def new
     @subject = Subject.new(name: 'Default')
+    @subject_count = Subject.count + 1
   end
 
   def create
@@ -18,12 +19,14 @@ class SubjectsController < ApplicationController
       flash[:notice] = "Subject created successfully."
       redirect_to(action: :index)
     else
+      @subject_count = Subject.count + 1
       render(:new)
     end
   end
 
   def edit
     @subject = Subject.find(params[:id])
+    @subject_count = Subject.count
   end
 
   def update
@@ -32,6 +35,7 @@ class SubjectsController < ApplicationController
       flash[:notice] = "Subject updated successfully."
       redirect_to(action: :show, id: @subject.id)
     else
+      @subject_count = Subject.count
       render(:edit)
     end
   end
@@ -50,6 +54,6 @@ class SubjectsController < ApplicationController
   private
 
   def subject_params
-    params.require(:subject).permit(:name, :position, :visible)
+    params.require(:subject).permit(:name, :position, :visible, :created_at)
   end
 end
