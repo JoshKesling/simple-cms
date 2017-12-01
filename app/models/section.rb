404e8af4f4c068f1802f2f2e7459b3f12df0bcd8
Josh Kesling <10089440+JoshKesling@users.noreply.github.com>
2017-12-01 21:46:56 +0800

    Add validation to forms

--- app/models/section.rb
+++ app/models/section.rb
@@ -3,6 +3,13 @@ class Section < ApplicationRecord
   has_many :editors, through: :section_edits, class_name: 'AdminUser'
   belongs_to :page
 
+  CONTENT_TYPES = %w[text HTML]
+
+  validates_presence_of :name
+  validates_length_of :name, maximum: 255
+  validates_inclusion_of :content_type, in: CONTENT_TYPES, message: "must be one of: #{CONTENT_TYPES.join(', ')}"
+  validates_presence_of :content
+
   scope :visible, -> {where(visible: true)}
   scope :invisible, -> {where(visible: false)}
   scope :sorted, -> {order('sections.position ASC')}
