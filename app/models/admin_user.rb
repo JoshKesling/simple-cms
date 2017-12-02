4bded1e830186a55f21cf518fc64ea3d44cc911f
Josh Kesling <10089440+JoshKesling@users.noreply.github.com>
2017-12-02 19:28:32 +0800

    Add CRUD for admin users

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,13 +1,14 @@
 class AdminUser < ApplicationRecord
-
   has_secure_password
 
   has_and_belongs_to_many :pages
   has_many :section_edits
-  has_many :sections, :through => :section_edits
+  has_many :sections, through: :section_edits
+
+  scope :sorted, -> { order('last_name ASC, first_name ASC') }
 
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
-  FORBIDDEN_USERNAMES = %w[littlebopeep humptydumpty marymary]
+  FORBIDDEN_USERNAMES = %w[littlebopeep humptydumpty marymary].freeze
 
   # validates_presence_of :first_name
   # validates_length_of :first_name, maximum: 25
@@ -22,16 +23,16 @@ class AdminUser < ApplicationRecord
   # validates_confirmation_of :email
 
   validates :first_name,  presence: true,
-                          length: {maximum: 25}
+                          length: { maximum: 25 }
 
   validates :last_name,   presence: true,
-                          length: {maximum: 50}
+                          length: { maximum: 50 }
 
-  validates :username,    length: {within: 4..25},
+  validates :username,    length: { within: 4..25 },
                           uniqueness: true
 
   validates :email,       presence: true,
-                          length: {maximum: 100},
+                          length: { maximum: 100 },
                           format: EMAIL_REGEX,
                           confirmation: true
 
@@ -39,8 +40,11 @@ class AdminUser < ApplicationRecord
 
   def username_is_allowed
     if FORBIDDEN_USERNAMES.include?(username)
-      errors.add(:username, " has been restricted from use.")
+      errors.add(:username, ' has been restricted from use.')
     end
   end
 
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
