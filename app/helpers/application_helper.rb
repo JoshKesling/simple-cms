ecb16a5c0d5d6387e8f06d19352043e87fe9e39a
Josh Kesling <10089440+JoshKesling@users.noreply.github.com>
2017-12-03 14:20:22 +0800

    Refactor code to use 'symbol: value' instead of ':symbol => value'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,9 @@ module ApplicationHelper
     options[:false_text] ||= ''
 
     if boolean
-      content_tag(:span, options[:true_text], :class => 'status true')
+      content_tag(:span, options[:true_text], class: 'status true')
     else
-      content_tag(:span, options[:false_text], :class => 'status false')
+      content_tag(:span, options[:false_text], class: 'status false')
     end
   end
 
